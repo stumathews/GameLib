4cfe1c860df193e4327d749d9c4a880137456365
Stuart Mathews <stuart@stuartmathews.com>
2020-03-18 22:04:42 +0000

    refactoring State class

--- GameLibFramework/Src/FSM/fsm.cs
+++ GameLibFramework/Src/FSM/fsm.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GameLibFramework.Src.FSM;
 using Microsoft.Xna.Framework;
 
-namespace GameLibFramework.Src.FSM
+namespace GameLibFramework.FSM
 {
     public class Transition
     {
@@ -21,29 +17,17 @@ namespace GameLibFramework.Src.FSM
     
     public abstract class State
     {
-        public State(string name)
+        protected State(string name)
         {
             Name = name;
         }
-        public virtual void Enter(object owner)
-        {
-            Console.WriteLine($"Entering State {Name}");
-        }
+        public virtual void Enter(object owner) => Console.WriteLine($"Entering State {Name}");
 
-        public virtual void Exit(object owner)
-        {
-            Console.WriteLine($"Exiting State {Name}");
-        }
+        public virtual void Exit(object owner) => Console.WriteLine($"Exiting State {Name}");
 
-        public virtual void Update(object owner, GameTime gameTime)
-        {
-            Console.WriteLine($"Entering Execute state of ' {Name}' with deltaTime of {gameTime.ElapsedGameTime}");
-        }
+        public virtual void Update(object owner, GameTime gameTime) => Console.WriteLine($"Entering Execute state of ' {Name}' with deltaTime of {gameTime.ElapsedGameTime}");
 
-        public virtual void Initialize()
-        {
-
-        }
+        public virtual void Initialize() { }
 
         public string Name
         {
@@ -51,67 +35,59 @@ namespace GameLibFramework.Src.FSM
             set;
         }
 
-        private List<Transition> m_Transitions = new List<Transition>();
+        private readonly List<Transition> _mTransitions = new List<Transition>();
 
-        public List<Transition> Transitions
-        {
-            get { return m_Transitions; }
-        }
+        public List<Transition> Transitions => _mTransitions;
 
-        public void AddTransition(Transition transition)
-        {
-            m_Transitions.Add(transition);
-        }
+        public void AddTransition(Transition transition) => _mTransitions.Add(transition);
     }
     
     public class FSM
     {
-        private object m_Owner;
-        private List<State> m_States;
-        private State m_CurrentState;
+        private readonly object _mOwner;
+        private readonly List<State> _mStates;
+        private State _mCurrentState;
         
         public FSM() : this(null) { }
 
         public FSM(object owner)
         {
-            m_Owner = owner;
-            m_States = new List<State>();
-            m_CurrentState = null;
+            _mOwner = owner;
+            _mStates = new List<State>();
+            _mCurrentState = null;
         }
 
         public void Initialise(string stateName)
         {
-            m_CurrentState = m_States.Find(state => state.Name.Equals(stateName));
-            if (m_CurrentState != null)
-            {
-                m_CurrentState.Enter(m_Owner);
-            }
+            _mCurrentState = _mStates.Find(state => state.Name.Equals(stateName));
+            _mCurrentState?.Enter(_mOwner);
         }
 
         public void AddState(State state)
         {
-            m_States.Add(state);
+            _mStates.Add(state);
         }
 
         public void Update(GameTime gameTime)
         {
             // Null check the current state of the FSM
-            if (m_CurrentState == null) return;
+            if (_mCurrentState == null) return;
+
             // Check the conditions for each transition of the current state
-            foreach (Transition t in m_CurrentState.Transitions)
+            foreach (var transition in _mCurrentState.Transitions)
             {
                 // If the condition has evaluated to true
                 // then transition to the next state
-                if (t.Condition())
+                if (transition.Condition())
                 {
-                    m_CurrentState.Exit(m_Owner);
-                    m_CurrentState = t.NextState;
-                    m_CurrentState.Enter(m_Owner);
+                    _mCurrentState.Exit(_mOwner);
+                    _mCurrentState = transition.NextState;
+                    _mCurrentState.Enter(_mOwner);
                     break;
                 }
             }
             // Execute the current state
-            m_CurrentState.Update(m_Owner, gameTime);
+            _mCurrentState.Update(_mOwner, gameTime);
         }    
     }        
 }
